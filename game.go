b6e9f91c460ead8327e1c34d58fed538fb7e35e0
marsxnow <raxelportiz@gmail.com>
2024-01-04 16:41:32 -0800

    Add command creation and handler functions, implement game logic

--- game.go
+++ game.go
@@ -1,32 +1,74 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 )
 
-type player struct {
-	id   string
+type Player struct {
+	ID   string
 	Name string
 }
 
-type game struct {
+type Game struct {
 	Player1 Player
 	Player2 Player
 }
 
-func NewGame(player1ID string, player1Name string, player2ID string, player2Name string) *Game {
-	return &Game{
-		Player1: Player{
-			id:   player1ID,
-			Name: player1Name,
-		},
-		Player2: Player{
-			id:   player2ID,
-			Name: player2Name,
-		},
-	}
+type Move int
+
+const (
+	Rock Move = iota
+	Paper
+	Scissors
+)
+
+func (m Move) String() string {
+	return [...]string{"Rock", "Paper", "Scissors"}[m]
 }
 
 func PlayGame(s *discordgo.Session, m *discordgo.MessageCreate, game *Game) {
+	// Send message to channel that game is starting
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Starting game between %s and %s", game.Player1.Name, game.Player2.Name))
 
+	// Wait for a message for either player
+	player1Move := waitForMove(s, game.Player1.ID)
+	player2Move := waitForMove(s, game.Player2.ID)
+
+	// Determine winner
+	winner := determineWinner(game, player1Move, player2Move)
+
+	// Announce Winner
+	if winner == game.Player1.ID {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s won!", game.Player1.Name))
+	} else if winner == game.Player2.ID {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s won!", game.Player2.Name))
+	} else {
+		s.ChannelMessageSend(m.ChannelID, "It's a tie!")
+	}
+}
+
+func waitForMove(s *discordgo.Session, playerID string) Move {
+	for {
+		msg, _ := s.ChannelMessage(playerID)
+		switch msg.Content {
+		case "rock":
+			return Rock
+		case "paper":
+			return Paper
+		case "scissors":
+			return Scissors
+		}
+	}
+}
+
+func determineWinner(game *Game, player1Move Move, player2Move Move) string {
+	if player1Move == player2Move {
+		return ""
+	} else if (player1Move == Rock && player2Move == Scissors) || (player1Move == Scissors && player2Move == Paper) || (player1Move == Paper && player2Move == Rock) {
+		return game.Player1.ID
+	} else {
+		return game.Player2.ID
+	}
 }
